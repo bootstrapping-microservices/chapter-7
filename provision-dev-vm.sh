0486a225e08578f75810374424dcfa3eda3c3447
Ashley Davis <ashley@codecapers.com.au>
2020-02-14 07:43:08 +1000

    Installed Kubectl in the Vagrant VM.

--- provision-dev-vm.sh
+++ provision-dev-vm.sh
@@ -50,3 +50,12 @@ docker --version
 sudo groupadd docker
 sudo gpasswd -a $USER docker
 sudo service docker restart
+
+#
+# Install Kubectl
+# https://kubernetes.io/docs/tasks/tools/install-kubectl/
+#
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.17.3/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
